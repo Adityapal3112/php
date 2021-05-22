5532f42d55648a712695485e361d54952349709a
Adityapal3112 <84620792+Adityapal3112@users.noreply.github.com>
2021-05-22 15:10:05 +0530

    Add files via upload

---
+++ dbconnect.php
@@ -0,0 +1,16 @@
+<?php
+
+$username = "root";
+$password = "";
+$database = "userinfo";
+$servername = "localhost";
+
+$conn = mysqli_connect($servername,$username,$password,$database);
+//   if($conn){
+//       echo"successssss";
+//   }
+//   else{
+//        die("error");
+// }
+
+?>
