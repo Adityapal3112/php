5c015ba47a71069d8f91bcc01fbcc3a14b7d2d12
exambuddy <84620792+exambuddy@users.noreply.github.com>
2022-03-12 08:17:15 +0530

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-session_unset();
-session_destroy();
-header("location:http://localhost/loginsystem/login.php");
-// echo "<script> location.href='http://localhost/loginsys/login.php'; </script> error";
-exit;
-?>
